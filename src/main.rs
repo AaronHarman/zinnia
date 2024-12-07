ed21b384152160eb2cd06d9967e0b0af758cff1b
Aaron Harman <aaron@aharman.com>
2024-12-06 19:14:30 -0500

    Added tray icon with which Zinnia can be closed

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 use std::process::Command;
 use std::io;
-use std::io::Write;
+use std::io::{Write, Cursor};
 use std::process::Stdio;
 use std::sync::mpsc;
 use std::thread;
 use std::collections::VecDeque;
 
-use cpal::Data;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 
 use rustpotter::{Rustpotter, RustpotterConfig};
 
+use tray_item::{IconSource, TrayItem};
+
+// Messages to be sent to the speech thread
+enum SpeakMessage<'a> {
+    Say(&'a str),
+}
+
+// Messages to be sent from the tray icon to the main program
+enum TrayMessage {
+    Close,
+}
 
 fn main() {
     // initialize rustpotter for wakeword detection
@@ -43,11 +53,15 @@ fn main() {
     let mut samples_buffer: VecDeque<f32> = VecDeque::new();
     let rp_buffer_size = rp.get_samples_per_frame();
 
+    // make a channel for sending messages to be spoken to the talk thread
+    let (send, recv) = mpsc::channel::<SpeakMessage>();
+    let in_send = send.clone(); // make another input for the in_stream to use
+
     // input device stuff
     let host = cpal::default_host();
     let in_device = host.default_input_device().expect("no input device available");
     println!("Input device: {}", in_device.name().unwrap());
-    let mut supported_configs_range = in_device.supported_input_configs()
+    let supported_configs_range = in_device.supported_input_configs()
         .expect("error while querying configs");
     let supported_config = supported_configs_range.filter(|x| x.sample_format() == cpal::SampleFormat::F32 && x.channels() == 2)
         .next()
@@ -67,6 +81,7 @@ fn main() {
                 let detection = rp.process_samples(samples_buffer.drain(..rp_buffer_size).collect());
                 if let Some(detection) = detection {
                     println!("Detected: {:?}", detection);
+                    let _ = in_send.send(SpeakMessage::Say("Zinnia here!"));
                 }
             }
         },
@@ -76,13 +91,22 @@ fn main() {
         },
         None // None=blocking, Some(Duration)=timeout
     );
+    // This is supposed to be here but it actually makes it not work for some reason so uhh yeah
+    //match in_stream {
+    //    Ok(is) => {
+    //        match is.play() { // it's possible the stream won't start automatically so this makes sure it does
+    //            Ok(_) => {},
+    //            Err(e) => {eprintln!("Error starting audio stream: {}", e);}
+    //        }
+    //    },
+    //    Err(e) => { eprintln!("Error making audio input stream: {}", e); }
+    //}
     
-    // make a channel for sending messages to be spoken to the talk thread
-    let (send, recv) = mpsc::channel::<&str>();
-
+    
     // make a thread to handle talking, and give it the receiver end of the channel
     let talk_thread = thread::spawn(move || {
-        for thing in recv {
+        for message in recv {
+            let SpeakMessage::Say(thing) = message;
             match say(thing.to_string()) {
                 Ok(_) => {},
                 Err(e) => {eprintln!("Error with speech synthesis: {}", e);}
@@ -90,13 +114,46 @@ fn main() {
         }
     });
 
-    //let _ = send.send("Zinnia here!");
-    //let _ = send.send("There are very few good reasons to skin a cat, but according to popular idioms there are quite a few methods to do so if you find you must.");
+    let _ = send.send(SpeakMessage::Say("Zinnia here!"));
+    let _ = send.send(SpeakMessage::Say("There are very few good reasons to skin a cat, but according to popular idioms there are quite a few methods to do so if you find you must."));
 
+    // get the icon to use in the tray
+    let img_decoder = png::Decoder::new(Cursor::new(include_bytes!("../resources/1f444.png")));
+    let (img_info, mut img_reader) = img_decoder.read_info().unwrap();
+    let mut img_buf = vec![0; img_info.buffer_size()];
+    img_reader.next_frame(&mut img_buf).unwrap();
+    let icon = IconSource::Data {
+        data: img_buf,
+        height: 72,
+        width: 72,
+    };
+    // set up the tray menu
+    let mut tray = TrayItem::new("Zinnia", icon).unwrap();
+    tray.add_label("ZINNIA").unwrap();
+    let (tray_tx, tray_rx) = mpsc::sync_channel::<TrayMessage>(2);
+    let tray_quit_tx = tray_tx.clone();
+    let id_menu = tray.inner_mut()
+        .add_menu_item_with_id("Close Zinnia", move || {
+            tray_quit_tx.send(TrayMessage::Close).unwrap();
+        }).unwrap();
+
+    println!("TEST: Made it to the tray message watch loop");
+    // watch for tray messages
+    loop {
+        match tray_rx.recv() {
+            Ok(TrayMessage::Close) => {
+                break;
+            }
+            Err(e) => {
+                eprintln!("Error with the tray menu: {}", e);
+            }
+        }
+    }
     
+    // gotta drop these two first so all the inputs to the speaking channel are closed
     drop(send);
+    drop(in_stream);
     talk_thread.join().expect("Error joining the talk thread");
-    while true {}
     
 }
 
